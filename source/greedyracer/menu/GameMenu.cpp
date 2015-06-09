ca30270d30a62972bbb34c9bb73e432ae0791963
EdwardPenishands <blackbird_X@web.de>
2015-06-09 22:49:53 +0200

    Game Menu eingefügt

--- source/greedyracer/menu/GameMenu.cpp
+++ source/greedyracer/menu/GameMenu.cpp
@@ -7,10 +7,10 @@ void CGameMenu::Init(CDeviceCursor * cursor)
 	this->cursor = cursor;
 	this->isVisible = true;
 
-	this->backgroundImage.Init("resources\\menu\\menubackground.png");
-	this->startButtonImage.Init("resources\\menu\\startButton.png");
-	this->creditsButtonImage.Init("resources\\menu\\creditsButton.png");
-	this->stopButtonImage.Init("resources\\menu\\beendenButton.png");
+	this->backgroundImage.Init("resources\\menu\\MenuScreen.jpg");
+	this->startButtonImage.Init("resources\\menu\\StartButton.jpg");
+	this->creditsButtonImage.Init("resources\\menu\\CreditsButton.jpg");
+	this->stopButtonImage.Init("resources\\menu\\EndButton.jpg");
 
 	this->rootOverlay.InitFull(&this->backgroundImage);
 	this->rootOverlay.SetLayer(0.8f);
@@ -19,16 +19,21 @@ void CGameMenu::Init(CDeviceCursor * cursor)
 	// die grafiken werden in einer auflösung von 180 pixeln gerade geliefert
 	// damit sie nicht skaliert werden bei einer auflösung von 1280 muss eine relative
 	// groesse von 0.14
-	float ratio = 16.0 / 9.0;
-	float buttonWidth = 0.14; // breite 
-	float buttonHeight = buttonWidth * ratio;
-	float buttonLeft = 0.5 - (buttonWidth / 2);
 
-	this->startButtonOverlay.Init(&this->startButtonImage, CFloatRect(buttonLeft, 0, buttonWidth, buttonHeight));
+	//float ratio = 16.0 / 9.0;
+	//float buttonWidth = 0.14; // breite 
+	//float buttonHeight = buttonWidth * ratio;
+	//float buttonLeft = 0.5 - (buttonWidth / 2);
+
+	float buttonWidth = 0.15f;
+	float buttonHeight = 0.075f;
+	float buttonLeft = 0.475f - (buttonWidth / 2);
+
+	this->startButtonOverlay.Init(&this->startButtonImage, CFloatRect(buttonLeft, 0.4, buttonWidth, buttonHeight));
 	//this->startButtonOverlay.SetLayer(0.5f);
-	this->creditsButtonOverlay.Init(&this->creditsButtonImage, CFloatRect(buttonLeft, 0.4, buttonWidth, buttonHeight));
+	this->creditsButtonOverlay.Init(&this->creditsButtonImage, CFloatRect(buttonLeft, 0.5, buttonWidth, buttonHeight));
 	//this->creditsButtonOverlay.SetLayer(0.5f);
-	this->stopButtonOverlay.Init(&this->stopButtonImage, CFloatRect(buttonLeft, 0.7, buttonWidth, buttonHeight));
+	this->stopButtonOverlay.Init(&this->stopButtonImage, CFloatRect(buttonLeft, 0.6, buttonWidth, buttonHeight));
 	//this->stopButtonOverlay.SetLayer(0.5f);
 
 	this->rootOverlay.AddOverlay(&this->startButtonOverlay);
